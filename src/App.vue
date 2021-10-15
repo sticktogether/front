02b069affa05ee511a29fb4d8e7ba4e49cd6f9fd
Igor <rullon2013@yandex.ru>
2021-10-15 10:56:51 +0300

    HW#1 Front

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <Flexcards />
+  </div>
+</template>
+
+<script>
+
+import  Flexcards from '@/components/Flexcards'
+export default {
+  name: 'App',
+  components: {
+    Flexcards
+  }
+}
+</script>
+
+<style lang="scss">
+* { 
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+*:focus {
+  outline: none;
+}
+
+body {
+  background: #EEEEEE;
+}
+
+</style>
